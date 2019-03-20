548a9c514950f0343a7133d52d5aae58f59cf24f
atichat lappanopakron <boss_atln@outlook.com>
2019-03-20 22:22:00 +0700

    enable cors on all

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const PORT = 8000;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.post('/api/account', (req, res) => {
     res.send({
