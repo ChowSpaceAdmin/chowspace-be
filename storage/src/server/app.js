c47716e6a53f9f6c54a9bd9e8dadb4344cf4ad5f
Virayut Sandhu <virayuts@gmail.com>
2019-06-03 20:25:11 +0700

    fix connection error

--- storage/src/server/app.js
+++ storage/src/server/app.js
@@ -1,24 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const config = require('./config');
 const storeRouter = require('../routers/StoreRouter');
 const fileRouter = require('../routers/FileRouter');
 const errorHandlers = require('../middlewares/ErrorHandlers');
 
 // Setup Mongo Database
-const MONGO_HOST = config.MONGO_HOST;
-const MONGO_PORT = config.MONGO_PORT;
-const MONGO_DATABASE = config.MONGO_DATABASE;
-const MONGO_USER = config.MONGO_USER;
-const MONGO_PASSWORD = config.MONGO_PASSWORD;
-
-mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}`, {
-    useNewUrlParser: true,
-    dbName: MONGO_DATABASE,
-    user: MONGO_USER,
-    pass: MONGO_PASSWORD,
-    useCreateIndex: true
-});
+require('./database');
 
 // Setup Express
 const app = express();
